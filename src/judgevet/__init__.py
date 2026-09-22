82557d2d99e3a5e1de0a7f1159582ba07484d66c
Alberto-Codes <alberto.nieto.80@gmail.com>
2026-09-22 05:37:34 -0700

    chore(main): release 0.4.0

--- src/judgevet/__init__.py
+++ src/judgevet/__init__.py
@@ -69,7 +69,7 @@ from judgevet.adapters.outbound.http import (
 )
 from judgevet.domain.questions import Choice, Noul, Score
 
-__version__ = "0.3.0"  # x-release-please-version
+__version__ = "0.4.0"  # x-release-please-version
 
 __all__ = [
     "AsyncHTTPSystemOneAdapter",
